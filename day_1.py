d50ac767465d4b3d75871008379712c86d92fe7a
Meliv <18529851+Meliv@users.noreply.github.com>
2024-12-01 16:09:12 +0000

    Day 1 dumb simple way and Day 2 setup

--- day_1.py
+++ day_1.py
@@ -1,5 +1,30 @@
 import file_reader
 
+def part_one(values: list[str]):
+    left = list()
+    right = list()
+
+    for value in values:
+        nums = value.split('   ')
+        left.append(int(nums[0]))
+        right.append(int(nums[1]))
+
+    left.sort()
+    right.sort()
+
+    sum = 0
+    for i in range(0, len(values)):
+        sum += abs(left[i] - right[i])
+
+    return sum
+
+def part_two(values: list[str]):
+    return 0
+
 values = file_reader.read_file('files/day1.txt')
 
-print(values)
+print(f"Part One: {part_one(values)}")
+print(f"Part Two: {part_two(values)}")
+
+
+print(sum)
