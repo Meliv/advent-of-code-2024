4797d139030942f479ac21883d580b55e620b36f
Meliv <18529851+Meliv@users.noreply.github.com>
2024-12-02 21:15:19 +0000

    Day 2 Part 2

--- day_2.py
+++ day_2.py
@@ -1,43 +1,25 @@
 import file_reader
-from itertools import islice, pairwise
-from collections import deque
+from itertools import pairwise
 
 FILE_NAME = 'files/day2.txt'
+pairwise_check = lambda v: all(1 <= abs(a-b) <= 3 for a, b in pairwise(v))
+
+input = [[int(i) for i in line] for line in [line.split() for line in file_reader.read_file(FILE_NAME)]]
 
 def part_one() -> int:
-    def pairwise_check(values) -> bool:
-        for a,b in pairwise(values):
-            if abs(a-b) < 1 or abs(a-b) > 3:
-                return False     
-        return True
-
-    return len(list(filter(lambda x: (sorted(x) == x or sorted(x, reverse=True) == x) and pairwise_check(x) , read_input())))
+    return len(list(filter(lambda x: (sorted(x) == x or sorted(x, reverse=True) == x) and pairwise_check(x), input)))
 
 def part_two() -> int:
-    def pairwise_check(values) -> bool:
-        
-        def get_window(values):
-            iterator = iter(values)
-            window = deque(islice(iterator, 2), maxlen=3)
-            for x in iterator:
-                window.append(x)
-                yield tuple(window)
+    valid_reports = 0
+    
+    for line in input:
+        excluded = [[x for j, x in enumerate(line) if j != i] for i in range(len(line))]
+    
+        find_valid_reports = lambda x: (sorted(x) == x or sorted(x, reverse=True) == x) and pairwise_check(x)
 
-        for a,b,c in get_window(values):
-            out_of_range = abs(a-b) < 1 or abs(a-b) > 3
-            if out_of_range and (abs(a-c) < 1 or abs(a-c) > 3):
-                print(f'{values} : False')
-                return False 
-            
-        print(f'{values} : True')
-        return True
+        valid_reports += int(any(filter(find_valid_reports, excluded)))
 
-    return len(list(filter(lambda x: pairwise_check(x) , read_input())))
-
-def read_input() -> list[list[int]]:
-    file_lines = file_reader.read_file(FILE_NAME)
-    return [[int(i) for i in line] for line in [line.split() for line in file_lines]]
-
+    return valid_reports
 
 print()
 print(f"Part One: {part_one()}")
