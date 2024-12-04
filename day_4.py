fc43924ea7b0ecfcbb1209500d40ed4ca8b1117c
Mark Thomas <mark.thomas@wealthify.com>
2024-12-04 08:23:05 +0000

    Setup for Day 4

---
+++ day_4.py
@@ -0,0 +1,12 @@
+import file_reader
+
+input = file_reader.read_file_as_str('files/day4.txt')
+
+def part_one() -> int:
+    return 0
+
+def part_two() -> int:
+    return 0
+
+print(f"Part One: {part_one()}")
+print(f"Part Two: {part_two()}")
---
+++ tests/test_day_4.py
@@ -0,0 +1,8 @@
+import day_4 as day, unittest
+
+class TestDay4(unittest.TestCase):
+    def test_part1(self):
+        self.assertEqual(day.part_one(), 0)
+
+    def test_part2(self):
+        self.assertEqual(day.part_two(), 0)
