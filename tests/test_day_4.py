491f4079f08caa8d6c14f038408aeae81cc588e0
Meliv <18529851+Meliv@users.noreply.github.com>
2024-12-04 18:28:41 +0000

    Day 4 Part 1

--- tests/test_day_4.py
+++ tests/test_day_4.py
@@ -6,36 +6,3 @@ class TestDay4(unittest.TestCase):
 
     def test_part2(self):
         self.assertEqual(day.part_two(), 0)
-
-    def test_get_diagonal_square(self):
-        diag = [
-            ['E','F','G','H','I'],
-            ['D','E','F','G','H'],
-            ['C','D','E','F','G'],
-            ['B','C','D','E','F'],
-            ['A','B','C','D','E']]
-        
-        self.assertEqual(''.join(day.get_diagonal(diag)), 'ABBCCCDDDDEEEEEIHHGGGFFFF')
-        
-    def test_get_diagonal_rectangle_tall(self):
-        diag = [
-            ['F','G','H'],
-            ['E','F','G'],
-            ['D','E','F'],
-            ['C','D','E'],
-            ['B','C','D'],
-            ['A','B','C']
-        ]
-        
-        self.assertEqual(''.join(day.get_diagonal(diag)), 'ABBCCCDDDEEEFFFHGG')
-
-    def test_get_diagonal_rectangle_wide(self):
-        diag = [
-            ['C','D','E','F','G','H','I'],
-            ['B','C','D','E','F','G','H'],
-            ['A','B','C','D','E','F','G']
-        ]
-        
-        self.assertEqual(''.join(day.get_diagonal(diag)), 'ABBCCCIHHGGGFFFEEEDDD')  
-        
-
