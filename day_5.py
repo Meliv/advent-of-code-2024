47d0233d22617b72d3c550093dfe4916b07ce8dc
Meliv <18529851+Meliv@users.noreply.github.com>
2024-12-05 22:20:06 +0000

    Midway checkin before I totally break this

--- day_5.py
+++ day_5.py
@@ -1,42 +1,78 @@
-from typing import DefaultDict, List, Tuple
 import file_reader
 from collections import defaultdict
 
 FILE_NAME = 'files/day5.txt'
-print('x')
 
-def parse_input() -> tuple[defaultdict[list[int]], list[list[int]]]:
+def get_valid_invalid() -> tuple[list[int],list[int]]:
     
-    orderings = defaultdict(list)
+    links = defaultdict(list)
     updates = list()
 
     for line in file_reader.read_file_as_list_str(FILE_NAME):
         if '|' in line:
             o = [int(x) for x in line.split('|')]
-            orderings[o[0]].append(o[1])
+            links[o[0]].append(o[1])
 
         if ',' in line:
             updates.append([int(x) for x in line.split(',')])
-    
-    return (orderings, updates)
 
-def part_one() -> int:
-    orderings, updates = parse_input()
     valid_updates = []
+    invalid_updates = []
 
     for update in updates:
         valid = True
         for i, n in enumerate(update):
-            if any([value for value in update[:i] if value in orderings[n]]):
+            if any([value for value in update[:i] if value in links[n]]):
                 valid = False
-                break
             
         if valid:
             valid_updates.append(update)
-
+        else:
+            invalid_updates.append(update)
+            
+    return valid_updates, invalid_updates
+    
+def part_one() -> int:
+    valid_updates, _ = get_valid_invalid()
     return sum([x[int((len(x)-1)/2)] for x in valid_updates])
 
 def part_two() -> int:
+    
+    visited = set() # Track visited nodes
+    topological_order = [] # Store topological order
+    graph = defaultdict(list[int])
+
+    def topological_sort_dfs(graph, node):
+
+        # Mark node as visited
+        visited.add(node)
+
+        # Recursively call DFS on its neighbors
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                    topological_sort_dfs(graph, neighbor)
+
+        # Add node to topological order
+        topological_order.append(node)
+    
+    ## Start
+    for i in range(100):
+        graph[i] = []
+
+    input = [75,97,47,61,53]
+
+    for line in file_reader.read_file_as_list_str(FILE_NAME):
+        if '|' in line:
+            o = [int(x) for x in line.split('|')]
+            if o[0] in input and o[1] in input:
+                graph[o[0]].append(o[1])
+    
+    for node in graph:
+        if node not in visited and any(graph[node]):
+            topological_sort_dfs(graph, node)
+            
+    print(list(reversed(topological_order)))
+            
     return 0
 
 print(f"Part One: {part_one()}")
