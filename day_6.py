ba29572c6b4e752fc17e7dbe3b57f4ba3b1497d3
Meliv <18529851+Meliv@users.noreply.github.com>
2024-12-06 08:32:16 +0000

    Day 6 setup

---
+++ day_6.py
@@ -0,0 +1,13 @@
+import file_reader
+from collections import defaultdict
+
+FILE_NAME = 'files/day6.txt'
+
+def part_one() -> int:
+    return 0
+
+def part_two() -> int:
+    return 0
+
+print(f"Part One: {part_one()}")
+print(f"Part Two: {part_two()}")
