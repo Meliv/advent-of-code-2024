4d411c8849da4252b315115ffeff036b2eda6105
Meliv <18529851+Meliv@users.noreply.github.com>
2024-12-07 13:44:06 +0000

    Day 7 Part 2. Ain't quick but it does the job

--- day_7.py
+++ day_7.py
@@ -18,9 +18,22 @@ def part_one():
         
     return sum(result)
 
-
 def part_two():
-    return 0
+    result, input = [], [(int(a.split(':')[0]),[int(b) for b in a.split(':')[1].split()]) for a in file_reader.read_file_as_list_str(FILE_NAME)]
+
+    for a,b in input:
+        for p in product([0,1,2], repeat=len(b)-1):
+            t = b[0]
+            for i,y in enumerate(b[1:]):
+                match list(p)[i]:
+                    case 2: t = int(str(t)+str(y))
+                    case 1: t = t*y
+                    case 0: t = t+y
+            if t == a:
+                result.append(a) 
+                break
+        
+    return sum(result)
 
 print(f"Part One: {part_one()}")
 print(f"Part Two: {part_two()}")
