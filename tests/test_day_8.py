f3744d825a1f1bc21fd32859a267d3049f7e5039
Meliv <18529851+Meliv@users.noreply.github.com>
2024-12-08 09:52:03 +0000

    Day 8 setup

---
+++ tests/test_day_8.py
@@ -0,0 +1,8 @@
+import day_8 as day, unittest
+
+class TestDay8(unittest.TestCase):
+    def test_part1(self):
+        self.assertEqual(day.part_one(), 0)
+
+    def test_part2(self):
+        self.assertEqual(day.part_two(), 0)
