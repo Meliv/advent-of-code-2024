4b74ae72ba23574d6e549a39e21f96f9c6f79995
Mark Thomas <mark.thomas@wealthify.com>
2024-12-09 11:27:29 +0000

    Messing with it between breaks

--- day_9.py
+++ day_9.py
@@ -4,8 +4,23 @@ FILE_NAME = 'files/day9.txt'
 
 def part_one():
     input = file_reader.read_file_as_str(FILE_NAME)
-    
-    return 0
+    print('x') #Debug
+
+    memory, defragged = [], []
+
+    for i,c in enumerate(input):
+        memory.extend(['.']*int(c) if i%2 else [str(i//2)]*int(c))
+
+    # Defrag here
+
+    for a in memory[::-1]:
+        print(a,end='')
+
+    print()
+
+    # End defrag
+
+    return sum((i*int(c) for i,c in enumerate(defragged) if c != '.'))
 
 def part_two():
     input = file_reader.read_file_as_str(FILE_NAME)
