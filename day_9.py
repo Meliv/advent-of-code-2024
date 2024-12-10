31bc6f0eb75b98dde027be0895c546a0a0366910
Meliv <18529851+Meliv@users.noreply.github.com>
2024-12-10 00:11:45 +0000

    Works. Can't make it work with the full input

--- day_9.py
+++ day_9.py
@@ -1,3 +1,4 @@
+import random
 import file_reader
 
 FILE_NAME = 'files/day9.txt'
@@ -29,33 +30,46 @@ def part_two():
     #memory[0] = 'Char . 0 9 etc'
     #memory[1] = 'Size of block'
     #memory[2] = 'Position in memory'
-    memory = [('.' if i%2 else str(i//2), int(c)) for i,c in enumerate(input)]
+    memory = [('.' if i%2 else str(i//2), int(c), i) for i,c in enumerate(input)]
     
     #print
-    x = [m[0]*m[1] for m in memory]
-    print(''.join(x))
+    #x = [m[0]*m[1] for m in memory]
+    #print(''.join(x))
+    
+    #print('-----')
     
     #0 char
     #1 size
     #2 position in memory-grouping
-    spaces = [(s[0],s[1],i) for i,s in enumerate(memory) if s[0] == '.']
-    files = [(f[0],f[1],i) for i,f in enumerate(memory) if f[0] != '.']
+    files = [(f[0],f[1]) for f in memory if f[0] != '.']
     
     for f in files[::-1]:
-        s = next((s for s in spaces if s[1] >= f[1]), None)
+        s = next(((s[0],s[1],i) for i,s in enumerate(memory) if s[0] == '.' and s[1] >= f[1]), None)
         if s is not None:
-            if s[2] >= f[2]: break
+            f_latest = next(len(memory)-i-1 for i,fm in enumerate(memory[::-1]) if fm[0] == f[0])
+            if f_latest <= s[2]: continue
 
+            memory[f_latest] = (s[0], f[1], f_latest)
 
+            # Insert file, insert new spacing if needed            
+            memory[s[2]] = (f[0],f[1],s[2])
+            if s[1] != f[1]:
+                memory.insert(s[2]+1, ('.',s[1]-f[1], s[2]+1))
 
-            pass
-                
-                
-    s = [m[0]*m[1] for m in memory]
+            
+            #print
+            #x = [m[0]*m[1] for m in memory]
+            #print(''.join(x))
     
-    print(''.join(s))
+                
+    #s = [m[0]*m[1] for m in memory]
+    
+    #print(''.join(s))
+    #print('----')
+    #print('00992111777.44.333....5555.6666.....8888..')
 
-    return sum((i*int(c[0]) for i,c in enumerate(memory) if c[0] != '.'))
+    m = ''.join([m[0]*m[1] for m in memory])
+    return sum((i*int(c) for i,c in enumerate(m) if c != '.'))
 
 print(f"Day 9")
 print(f"Part One: {part_one()}")
