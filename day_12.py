3454902fceb0ce471204c28600d556852b7da30f
Mark Thomas <mark.thomas@wealthify.com>
2024-12-12 19:10:08 +0000

    Swapping pcs

--- day_12.py
+++ day_12.py
@@ -3,6 +3,15 @@ import file_reader
 FILE_NAME = 'files/day12.txt'
 
 def part_one():
+
+    input = file_reader.read_file_as_list_str(FILE_NAME)
+
+    # Add padding
+    for i in range(len(input)):
+        input[i] = '.'+''.join(input[i])+'.'
+    input.insert(0, '.'*len(input[0]))
+    input.insert(len(input), '.'*len(input[0]))
+
     return 0
 
 def part_two():
