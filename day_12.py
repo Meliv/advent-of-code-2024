ab8f292c17cf806ad48444074aead254f6707a86
Meliv <18529851+Meliv@users.noreply.github.com>
2024-12-12 19:58:40 +0000

    About to do something dumb

--- day_12.py
+++ day_12.py
@@ -12,6 +12,46 @@ def part_one():
     input.insert(0, '.'*len(input[0]))
     input.insert(len(input), '.'*len(input[0]))
 
+    '''
+    Flood-fill (node):
+    1. If node is not Inside return.
+    2. Set the node
+    3. Perform Flood-fill one step to the south of node.
+    4. Perform Flood-fill one step to the north of node
+    5. Perform Flood-fill one step to the west of node
+    6. Perform Flood-fill one step to the east of node
+    7. Return.
+    '''
+    
+    processed_nodes = []
+    shapes = []
+    
+    def flood_fill(input, x, y, c, filled_nodes):
+        if input[y][x] != c or (x,y) in processed_nodes:
+            return filled_nodes
+        
+        processed_nodes.append((x,y))
+        
+        flood_fill(input, x, y+1, c, filled_nodes)
+        flood_fill(input, x, y-1, c, filled_nodes)
+        flood_fill(input, x-1, y, c, filled_nodes)
+        flood_fill(input, x+1, y, c, filled_nodes)
+        
+        filled_nodes.append((x,y))
+        return filled_nodes
+
+    for y in range(len(input)):
+        for x in range(len(input[y])):
+            if input[y][x] == '.' or (x,y) in processed_nodes:
+                continue
+            
+            filled_nodes = flood_fill(input, x, y, input[y][x], [])
+            
+            if any(filled_nodes):
+                shapes.append(filled_nodes)
+
+    # Work out perimeters here
+    
     return 0
 
 def part_two():
